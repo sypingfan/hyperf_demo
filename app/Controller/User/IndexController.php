e98c91ec7d4b91cd7cfdec20622aceabfeacaae4
刘平凡 <liu2476979217@163.com>
2023-10-13 22:04:14 +0800

    PHP Annotations PHPstorm安装这个扩展自动引入命名空间和注解等功能‘、路由注解

--- app/Controller/User/IndexController.php
+++ app/Controller/User/IndexController.php
@@ -9,10 +9,18 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-namespace App\Controller;
+namespace App\Controller\User;
 
+
+use App\Controller\AbstractController;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\RequestMapping;
+
+#[Controller(prefix: "index")]
 class IndexController extends AbstractController
 {
+
+    #[RequestMapping(path: "override",methods: ["get","post"])]
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
